deb035c09a65efae5be5581eee3a63fc9638047a
hyj18210653976-debug <hyj18210653976@gmail.com>
2026-09-16 15:52:07 +0800

    Create F346A_multilocation_event_role.R
    Archive the F346A review of 15 multi-location homicide cases, distinguishing killing-site evidence from dump, transport, and discovery-site evidence for county-level exposure assignment.

---
+++ archive/F346A_multilocation_event_role.R
@@ -0,0 +1,13 @@
+Target exposure location = killing site
+NOT dump site
+NOT transport site
+NOT body-discovery site
+
+15 multi-location targets
+
+A_KILL_ROLE_EVIDENCE_FOUND = 0
+B_ONLY_NONKILL_ROLE_EVIDENCE = 1
+C_NO_EVENT_ROLE_EVIDENCE = 14
+
+松北区 = 抛尸地
+
